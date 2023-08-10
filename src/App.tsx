4d39dc40804953be322cd3fe3269e349e2e23738
Vad197779 <kovtunvad@gmail.com>
2023-08-10 14:55:59 +0300

    theme.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import { StyledBtn } from './components/Button.styled';
+import { Link } from './components/Link.styled';
+import { myTheme } from './styles/Theme.styled';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Menu>
+        <ul>
+          <li><a href="">menu item 1</a></li>
+          <li><a href="">menu item 2</a></li>
+          <li><a href="">menu item 3</a></li>
+        </ul>
+      </Menu> */}
+
+      <Box>
+        {/* <StyledBtn as={Link} href={"#"}>LinkComponent</StyledBtn> */}
+        {/* <StyledBtn as="a" href={"#"}>Link</StyledBtn> */}
+        {/* <StyledBtn color='green' fontSize="40px">Hello</StyledBtn>
+        <StyledBtn color='red'>Hello</StyledBtn>
+        <StyledBtn fontSize='30px'>Hello</StyledBtn> */}
+        <StyledBtn color={myTheme.colors.primary} btnType={"primary"} active>Hello</StyledBtn>
+        <StyledBtn color={myTheme.colors.secondary} btnType={"outlined"}>Hello</StyledBtn>
+        {/* <SuperButton>Super Button</SuperButton> */}
+        {/* <input type='text' />
+        <input type='submit' />
+        <input type='radio' />
+        <input type='checkbox' /> */}
+      </Box>
     </div>
   );
 }
 
 export default App;
+
+const Box = styled.div `
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  button {
+    cursor: pointer;
+  }
+
+  ${Link} {
+    cursor: zoom-in;
+  }
+
+  @media ${myTheme.media.tablet} {
+    flex-direction: column;
+  }
+`

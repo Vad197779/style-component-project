46bdc8ae05182e9d14b287c63de0d2a885503534
Vad197779 <kovtunvad@gmail.com>
2023-08-10 14:49:08 +0300

    menu.

---
+++ src/styles/Theme.styled.tsx
@@ -0,0 +1,14 @@
+export const myTheme = {
+  colors: {
+    primary: "gold",
+    secondary: "lightgreen",
+    grey: {
+      dark: "#2b2929",
+      light: "#bebaba"
+    }
+  },
+  media: {
+    tablet: "screen and (max-width: 768px) and (min-width: 577px)",
+    mobile: "screen and (max-width: 576px)",
+  }
+}
